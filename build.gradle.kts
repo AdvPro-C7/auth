ccfe250a93439bae95b5f1b57e4c30ccb546d412
bryan-ilman-on-github <bryanilman20@gmail.com>
2024-04-17 18:17:54 +0700

    [TEST] tests creation for controller

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,20 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("jacoco")
+    id("java")
+    id("org.sonarqube") version "3.0"
+    id("org.springframework.boot") version "3.0.5"
+}
+
+jacoco {
+	toolVersion = "0.8.10"
 }
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
@@ -22,15 +28,37 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	annotationProcessor("org.projectlombok:lombok")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	implementation("io.micrometer:micrometer-registry-prometheus")
+	implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
+	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+	implementation("javax.xml.bind:jaxb-api:2.3.1")
+	implementation("mysql:mysql-connector-java:8.0.32")
+	implementation("org.postgresql:postgresql:42.6.0")
+	implementation("org.projectlombok:lombok")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.security:spring-security-config:6.0.2")
+	testImplementation("junit:junit:4.13.2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks.named("test") {
+	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test) // tests are required to run before generating the report
+	reports {
+		xml.required.set(true)
+	}
 }
---
+++ src/test/java/id/ac/ui/cs/advprog/auth/controller/AuthenticationControllerTest.java
@@ -0,0 +1,132 @@
+package id.ac.ui.cs.advprog.auth.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.ac.ui.cs.advprog.auth.model.User;
+import id.ac.ui.cs.advprog.auth.model.request.LoginRequest;
+import id.ac.ui.cs.advprog.auth.model.request.RegisterRequest;
+import id.ac.ui.cs.advprog.auth.service.invoker.AuthenticationInvokerImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class AuthenticationControllerTest {
+        @Autowired
+        private AuthenticationController controller;
+
+        @Mock
+        private AuthenticationInvokerImpl service;
+
+        private MockMvc design;
+
+        @BeforeEach
+        public void setUp() {
+                ReflectionTestUtils.setField(this.controller, "service", this.service);
+                this.design = MockMvcBuilders.standaloneSetup(this.controller).build();
+        }
+
+        @Test
+        void testRegisterUserSuccess() throws Exception {
+                // Arrange
+                Mockito.when(this.service.insertUser(Mockito.any(RegisterRequest.class))).thenReturn(true);
+
+                // Act and Assert
+                this.design.perform(post("/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(
+                                                new RegisterRequest("bryan", "bryanilman20@gmail.com", "00008888",
+                                                                "helloworld"))))
+                                .andExpect(status().isOk())
+                                .andExpect(content().string("{\"message\":\"registration successful\"}"));
+
+                // Verify
+                Mockito.verify(this.service, atLeastOnce()).insertUser(Mockito.any(RegisterRequest.class));
+        }
+
+        @Test
+        void testRegisterUserBadRequest() throws Exception {
+                // Arrange
+                Mockito.when(this.service.insertUser(Mockito.any(RegisterRequest.class))).thenReturn(false);
+
+                // Act and Assert
+                this.design.perform(post("/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(
+                                                new RegisterRequest("bryan", "bryanilman20@gmail.com", "00008888",
+                                                                "helloworld"))))
+                                .andExpect(status().isBadRequest())
+                                .andExpect(content().string("{\"message\":\"registration failed\"}"));
+
+                // Verify
+                Mockito.verify(this.service, atLeastOnce()).insertUser(Mockito.any(RegisterRequest.class));
+        }
+
+        @Test
+        void testLoginUserSuccess() throws Exception {
+                // Arrange
+                var loginRequest = new LoginRequest("bryanilman20@gmail.com", "helloworld");
+                Mockito.when(this.service.authenticateUser(Mockito.any(LoginRequest.class))).thenReturn(true);
+
+                ResponseCookie token = ResponseCookie.from("buku-id-session", "jwt").build();
+                Mockito.when(this.service.createToken(loginRequest.getId())).thenReturn(token);
+
+                // Act and Assert
+                this.design.perform(post("/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(loginRequest)))
+                                .andExpect(status().isOk())
+                                .andExpect(content().string("{\"message\":\"login successful\"}"));
+
+                // Assert
+                Mockito.verify(this.service, atLeastOnce()).authenticateUser(Mockito.any(LoginRequest.class));
+        }
+
+        @Test
+        void testLogUserInBadRequest() throws Exception {
+                // Arrange
+                Mockito.when(this.service.authenticateUser(Mockito.any(LoginRequest.class))).thenReturn(false);
+
+                // Act
+                this.design.perform(post("/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper()
+                                                .writeValueAsString(new LoginRequest("bryanilman20@gmail.com",
+                                                                "helloworld"))))
+                                .andExpect(status().isBadRequest())
+                                .andExpect(content().string("{\"message\":\"invalid credentials\"}"));
+
+                // Assert
+                Mockito.verify(this.service, atLeastOnce()).authenticateUser(Mockito.any(LoginRequest.class));
+        }
+
+        @Test
+        void testAuthAndGetUserDetails() throws Exception {
+                // Arrange
+                var userDetails = new User("bryan", "bryanilman20@gmail.com", "00008888", "helloworld");
+                Mockito.when(this.service.getUserDetails(Mockito.any(MockHttpServletRequest.class)))
+                                .thenReturn(userDetails);
+
+                // Act
+                this.design.perform(get("/authenticate"))
+                                .andExpect(status().isOk())
+                                .andExpect(content().json("{\"user-details\": \"" + userDetails.toString() + "\"}"));
+
+                // Assert
+                Mockito.verify(this.service, atLeastOnce()).getUserDetails(Mockito.any(MockHttpServletRequest.class));
+        }
+}
