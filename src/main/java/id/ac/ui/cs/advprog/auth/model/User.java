13c60461e7f1d4b178b5d42f731e2fddef0f23f4
fikrirmdhna <fikrirmdhna01@gmail.com>
2024-04-24 20:43:49 +0700

    [REFACTOR] User model

--- src/main/java/id/ac/ui/cs/advprog/auth/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/auth/model/User.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 @Entity
 @Table(name = "users", uniqueConstraints = {
