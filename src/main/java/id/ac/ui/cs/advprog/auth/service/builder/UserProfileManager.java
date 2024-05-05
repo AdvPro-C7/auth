4e5e7bc2564b5b80589b6afa725fb05f52137a87
fikrirmdhna <fikrirmdhna01@gmail.com>
2024-05-05 11:52:05 +0700

    [GREEN] Implement UserProfileBuilder, Manager, and DTO

--- src/main/java/id/ac/ui/cs/advprog/auth/service/builder/UserProfileManager.java
+++ src/main/java/id/ac/ui/cs/advprog/auth/service/builder/UserProfileManager.java
@@ -7,23 +7,44 @@ import id.ac.ui.cs.advprog.auth.model.User;
 public class UserProfileManager {
 
     public User constructProfilInfo(User user) {
+        return new UserProfileBuilder(user)
+                .updateInfo()
+                .build();
     }
 
     public User constructPhotoProfile(User user, byte[] newPhoto) {
+        return new UserProfileBuilder(user)
+                .updatePhoto(newPhoto)
+                .build();
     }
 
     public User constructPasswordProfile(User user, String newPassword) {
+        return new UserProfileBuilder(user)
+                .updatePassword(newPassword)
+                .build();
     }
 
     public User constructNameProfile(User user, String newName) {
+        return new UserProfileBuilder(user)
+                .updateName(newName)
+                .build();
     }
 
     public User constructBirthDateProfile(User user, LocalDate newBirthDate) {
+        return new UserProfileBuilder(user)
+                .updateBirthDate(newBirthDate)
+                .build();
     }
 
     public User constructBioProfile(User user, String newBio) {
+        return new UserProfileBuilder(user)
+                .updateBio(newBio)
+                .build();
     }
 
     public User constructGenderProfile(User user, String newGender) {
+        return new UserProfileBuilder(user)
+                .updateGender(newGender)
+                .build();
     }
 }
--- src/main/java/id/ac/ui/cs/advprog/auth/service/builder/UserProfileUpdateDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/auth/service/builder/UserProfileUpdateDTO.java
@@ -15,33 +15,51 @@ public class UserProfileUpdateDTO {
     public UserProfileUpdateDTO() {}
 
     public UserProfileUpdateDTO(String name, LocalDate birthDate, String bio, String gender, String password, MultipartFile photo) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.bio = bio; 
+        this.gender = gender;
+        this.password = password;
+        this.photo = photo;
     }
 
     // Setters
     public void setName(String newName) {
+        this.name = newName;
     }
     public void setBirthDate(LocalDate newDate) {
+        this.birthDate = newDate;
     }
     public void setBio(String newBio) {
+        this.bio = newBio;
     }
     public void setGender(String newGender) {
+        this.gender = newGender;
     }
     public void setPassword(String newPassword) {
+        this.password = newPassword;
     }
     public void setPhoto(MultipartFile newPhoto) {
+        this.photo = newPhoto;
     }
 
     // Getters
     public String getName() {
+        return name;
     }
     public LocalDate getBirthDate() {
+        return birthDate;
     }
     public String getBio() {
+        return bio;
     }
     public String getGender() {
+        return gender;
     }
     public String getPassword() {
+        return password;
     }
     public MultipartFile getPhoto() {
+        return photo;
     }
 }
