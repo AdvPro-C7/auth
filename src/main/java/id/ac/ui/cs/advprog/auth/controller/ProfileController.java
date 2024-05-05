e879596ab46f8321e4066392948c56938d87bf97
fikrirmdhna <fikrirmdhna01@gmail.com>
2024-05-05 16:55:01 +0700

    [GREEN] Implement Controller for Profile Update

--- src/main/java/id/ac/ui/cs/advprog/auth/controller/ProfileController.java
+++ src/main/java/id/ac/ui/cs/advprog/auth/controller/ProfileController.java
@@ -20,5 +20,27 @@ public class ProfileController {
     public ResponseEntity<User> updateProfile(
         @RequestPart("updates") UserProfileUpdateDTO updates,
         @RequestBody User user) throws Exception {
+
+        if (updates.getName() != null) {
+            user = userProfileManager.constructNameProfile(user, updates.getName());
+        }
+        if (updates.getBirthDate() != null) {
+            user = userProfileManager.constructBirthDateProfile(user, updates.getBirthDate());
+        }
+        if (updates.getBio() != null) {
+            user = userProfileManager.constructBioProfile(user, updates.getBio());
+        }
+        if (updates.getGender() != null) {
+            user = userProfileManager.constructGenderProfile(user, updates.getGender());
+        }
+        if (updates.getPassword() != null) {
+            user = userProfileManager.constructPasswordProfile(user, updates.getPassword());
+        }
+        if (updates.getPhoto() != null) {
+            byte[] newPhoto = updates.getPhoto().getBytes();
+            user = userProfileManager.constructPhotoProfile(user, newPhoto);
+        }
+
+        return ResponseEntity.ok(user);
     }
 }
