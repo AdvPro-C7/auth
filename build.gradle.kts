98dff3ace978ce9317eb3083d1662206a5edffe5
bryan-ilman-on-github <bryanilman20@gmail.com>
2024-05-22 08:35:46 +0700

    [IMPLEMENT] deploy to GCP Cloud Run in Bryan's account

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,22 @@
 plugins {
-    id("io.spring.dependency-management") version "1.1.0"
+    id("io.spring.dependency-management") version "1.1.5"
     id("jacoco")
     id("java")
-    id("org.sonarqube") version "3.0"
-    id("org.springframework.boot") version "3.0.5"
+    id("org.sonarqube") version "5.0.0.4638"
+    id("org.springframework.boot") version "3.2.5"
 }
 
 jacoco {
-	toolVersion = "0.8.10"
+	toolVersion = "0.8.12"
 }
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 configurations {
@@ -32,24 +34,24 @@ dependencies {
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	implementation("io.jsonwebtoken:jjwt:0.9.0")
 	implementation("io.micrometer:micrometer-registry-prometheus")
-	implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
-	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
-	implementation("javax.xml.bind:jaxb-api:2.3.1")
+	implementation("io.micrometer:micrometer-registry-prometheus")
+	implementation("jakarta.servlet:jakarta.servlet-api")
+	implementation("jakarta.validation:jakarta.validation-api")
+	implementation("javax.xml.bind:jaxb-api:2.3.0")
 	implementation("mysql:mysql-connector-java:8.0.32")
-	implementation("org.postgresql:postgresql:42.6.0")
+	implementation("org.postgresql:postgresql")
 	implementation("org.projectlombok:lombok")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("io.micrometer:micrometer-registry-prometheus")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.security:spring-security-config:6.0.2")
-	testImplementation("junit:junit:4.13.2")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.security:spring-security-config")
+	testImplementation("junit:junit")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
